3391966daec362cedfe616fb5455d5875b7d41d5
Maxim <pogchamp1401@gmail.com>
2023-04-30 00:51:42 +0300

    add default

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images:
+      {
+        domains: [
+            'cdn2.thecatapi.com',
+            '28.media.tumblr.com',
+            '24.media.tumblr.com',
+            '25.media.tumblr.com',
+            '26.media.tumblr.com',
+            '30.media.tumblr.com',
+            '29.media.tumblr.com',
+            '27.media.tumblr.com'
+        ],
+      },
 }
 
 module.exports = nextConfig
---
+++ src/pages/cat/[id].tsx
@@ -0,0 +1,32 @@
+import { useRouter } from 'next/router'
+
+const CatDetail = ({cat}) => {
+
+    console.log(cat)
+
+    return (
+        <div> {cat.id} </div>
+    )
+}
+
+
+export async function getServerSideProps(context) {
+
+    const id = context.query.id
+
+
+
+    const response = await fetch(`${process.env.NEXT_PUBLIC_CAT_API}/images/${id}`, {
+        headers: {
+            'x-api-key': 'live_cdDyPkqKPEm7J3IiMV6e31uVHpneQOey2j6Bfw3xprkRJdzp3pMcsG1euDSVHfbb',
+        }
+    })
+
+    const cat = await response.json()
+
+    return {
+        props: {cat}, // will be passed to the page component as props
+    }
+}
+
+export default CatDetail
