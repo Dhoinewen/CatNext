0f7c8d678422653fa13bd7fb9806563fd1a8bf2c
Maxim <pogchamp1401@gmail.com>
2023-05-02 21:29:06 +0300

    cat detail prettier

--- src/pages/cat/[id].tsx
+++ src/pages/cat/[id].tsx
@@ -1,28 +1,28 @@
-import {CatType} from "@/types/main";
+import { CatType } from "@/types/main";
 
-const CatDetail = ({cat}: {cat: CatType}) => {
-
-    return (
-        <div> {cat.id} </div>
-    )
-}
-
+const CatDetail = ({ cat }: { cat: CatType }) => {
+  return <div> {cat.id} </div>;
+};
 
 export async function getServerSideProps(context: any) {
+  const id = context.query.id;
 
-    const id = context.query.id
-
-    const response = await fetch(`${process.env.NEXT_PUBLIC_CAT_API}/images/${id}`, {
-        headers: {
-            'x-api-key': process.env.NEXT_PUBLIC_KEY ? process.env.NEXT_PUBLIC_KEY : "noKey"
-        }
-    })
-
-    const cat = await response.json()
-
-    return {
-        props: {cat},
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_CAT_API}/images/${id}`,
+    {
+      headers: {
+        "x-api-key": process.env.NEXT_PUBLIC_KEY
+          ? process.env.NEXT_PUBLIC_KEY
+          : "noKey",
+      },
     }
+  );
+
+  const cat = await response.json();
+
+  return {
+    props: { cat },
+  };
 }
 
-export default CatDetail
+export default CatDetail;
