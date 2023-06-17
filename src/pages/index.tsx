f59519ba2eaf9163a58c33a398226e962c30e38c
Maxim <pogchamp1401@gmail.com>
2023-06-18 00:25:51 +0300

    tests

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,7 +19,7 @@ function Home({ test, breeds }: { test: CatType[]; breeds: Breed[] }) {
           {breeds.map((breed, index) => (
             <button
               className={clsx(
-                "py-2 px-3 rounded-xl mb-4 mr-4 border",
+                "py-2 px-3 rounded-xl mb-4 mr-4 border dark:bg-vanila",
                 breedsFilter.includes(breed.id)
                   ? "bg-vanila"
                   : "bg-beige"
